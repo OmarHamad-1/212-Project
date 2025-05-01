0424353f8e50b99ff6ab3058b1b29f2bdda7f9ba
Nawaf Alwazrah <nwazrah@gmail.com>
2025-05-01 23:51:44 +0300

    add head of functions nvIndexPhotoManager.java

--- InvIndexPhotoManager.java
+++ InvIndexPhotoManager.java
@@ -1,3 +1,11 @@
 InvIndexPhotoManager{
 
+  public Photomanager();
+
+  public void addPhoto(Photo p);
+
+  public void deletePhoto(String path);
+
+  public BST<LinkedList<Photo>> getPhotos();
+
 }
