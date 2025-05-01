82f9773b12aa1d0ffae48950f7b88f520f6625db
Nawaf Alwazrah <nwazrah@gmail.com>
2025-05-01 23:48:01 +0300

    add head of functins in PhotoManager.java class

--- PhotoManager.java
+++ PhotoManager.java
@@ -1,3 +1,15 @@
 public class PhotoManager{
+
+LinkedList<Photo> Photos;  
+  
+  public PhotoManager();
+
+  public LinkedList<Photo> getPhotos();
+
+  public void addPhoto(Photo p);
+
+  public void deletePhoto(String path);
+
+  public boolean Photo_Exist(String path):
   
 }
