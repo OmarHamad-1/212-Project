cce2c11aa5604d7f9136b351d3e7a4ad105f300f
ibrahimaldobiyan <ibrahimaldobiyan10@gmail.com>
2025-05-03 12:38:31 +0300

    Update BSTNode.java

--- BST/BSTNode.java
+++ BST/BSTNode.java
@@ -1,1 +1,9 @@
-
+class BSTNode <T> {
+	public String key;
+	public T data;
+	public BSTNode<T> left, right;
+	public BSTNode(String key, T val) {
+		this.key = key;
+		data = val;
+		left = right = null;
+	}
